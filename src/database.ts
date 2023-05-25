66614f50796df7a227466f0b4e56e95604fcc69b
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-05-25 12:58:09 -0300

    refactor: creates config variable

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,11 @@
 import { knex as setupKnex } from 'knex'
 
-export const knex = setupKnex({
+export const config = {
   client: 'sqlite3',
   connection: {
     filename: './mydb.sqlite',
   },
   useNullAsDefault: true,
-})
+}
+
+export const knex = setupKnex(config)
