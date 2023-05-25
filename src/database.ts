15a9b89ca26dcc9167c75512e75d7d59a1a9696b
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-05-25 12:49:51 -0300

    config: setup knex

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,8 @@
 import { knex as setupKnex } from 'knex'
 
 export const knex = setupKnex({
-  client: 'sqlite',
+  client: 'sqlite3',
   connection: {
-    filename: './tmp/app.db',
+    filename: './mydb.sqlite',
   },
 })
